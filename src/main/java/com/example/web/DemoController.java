20e76f82d8cecd58b31a383f569dafe153dcd645
Mohsen <mohsen.mohammadi.se@gmail.com>
2018-07-22 00:14:51 +0430

    Interactivity included

--- src/main/java/com/example/web/DemoController.java
+++ src/main/java/com/example/web/DemoController.java
@@ -2,9 +2,12 @@ package com.example.web;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping
@@ -13,4 +16,48 @@ public class DemoController{
     public LocalDateTime sayTheTime(){
         return LocalDateTime.now();
     }
+
+    @RequestMapping(path = "many")
+    public String sayTheTimeMany(@RequestParam() String name, @RequestParam(defaultValue = "10") int repetitions) {
+        return IntStream.rangeClosed(1, repetitions).mapToObj(i->i+ " .Hello "+ name +" !Now It Is "+ LocalDateTime.now())
+                .collect(Collectors.joining(System.lineSeparator()));
+
+    }
+    @RequestMapping(path = "manyParams")
+    public String sayTheTimeManyParams(Params params) {
+        return IntStream.rangeClosed(1, params.repetitions).mapToObj(i -> i + " .Hello " + params.name + " !Now It Is " + LocalDateTime.now())
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    @RequestMapping(path = "many/{name}/{repetitions}")
+    public String sayTheTimeManyParamsPath(Params params) {
+        return IntStream.rangeClosed(1, params.repetitions).mapToObj(i -> i + " .Hello " + params.name + " !Now It Is " + LocalDateTime.now())
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+
+    static class Params{
+        String name;
+        int repetitions;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getRepetitions() {
+            return repetitions;
+        }
+
+        public void setRepetitions(int repetitions) {
+            this.repetitions = repetitions;
+        }
+
+    }
+
+
+
 }
